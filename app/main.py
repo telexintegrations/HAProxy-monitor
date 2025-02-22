2877f673963ab0cb8fc478d2678e50ac45d7a39c
0xnuru <0xnuru@gmail.com>
2025-02-22 13:20:25 +0100

    feat: add integration router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import hello_world
+from app.routers import integration
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -8,16 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "https://www.madeformore.ca",
-        "https://madeformore.ca",
-        "https://made-for-more.onrender.com",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(hello_world.router)
+app.include_router(integration.router)
--- app/routers/hello_world.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-
-
-router = APIRouter(prefix="", tags=["Hello World"])
-
-@router.get("/")
-async def get_root():   
-    return {"message": "Hello World"}
